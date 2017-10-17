5af82650c0dd0934709d883ca79833ed30c303dd
JanuriDP <janury.geek@gmail.com>
2017-10-17 16:00:38 +0700

    templating

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,8 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome');
+		$this->load->view('templates/vHeader');
+		$this->load->view('home/vBody');
+		$this->load->view('templates/vFooter');
 	}
 }
---
+++ application/views/templates/vFooter.php
@@ -0,0 +1,63 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!doctype html>
+        <footer class="mdl-mega-footer">
+          <div class="mdl-mega-footer--middle-section">
+            <div class="mdl-mega-footer--drop-down-section">
+              <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
+			  <h1 class="mdl-mega-footer--heading">Kontak</h1>
+              <ul class="mdl-mega-footer--link-list">
+				<li><i class="material-icons">location_on</i> Peninggaran, Keb. Lama Utara, Jakarta</li>
+				<li><i class="material-icons">phone</i> Phone: +6285775254325</li>
+				<li><i class="material-icons">email</i> Email: info@peninggaran.com</li>
+              </ul>
+            </div>
+            <div class="mdl-mega-footer--drop-down-section">
+              <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
+              <h1 class="mdl-mega-footer--heading">Profile</h1>
+              <ul class="mdl-mega-footer--link-list">
+                <li><a href="#">Visi & Misi</a></li>
+                <li><a href="#">Struktur Organisasi</a></li>
+              </ul>
+            </div>
+            <div class="mdl-mega-footer--drop-down-section">
+              <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
+              <h1 class="mdl-mega-footer--heading">Informasi</h1>
+              <ul class="mdl-mega-footer--link-list">
+                <li><a href="#">Agenda</a></li>
+                <li><a href="#">Pengumuman</a></li>
+                <li><a href="#">Anggaran/Kas</a></li>
+				<li><a href="#">Statistik</a></li>
+              </ul>
+            </div>
+            <div class="mdl-mega-footer--drop-down-section">
+              <input class="mdl-mega-footer--heading-checkbox" type="checkbox" checked>
+              <h1 class="mdl-mega-footer--heading">FAQ</h1>
+              <ul class="mdl-mega-footer--link-list">
+                <li><a href="#">Questions</a></li>
+                <li><a href="#">Answers</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="mdl-mega-footer--bottom-section">
+            <div class="mdl-logo">
+              &copy; <?=date('Y');?> Kampung Peninggaran
+            </div>
+            <ul class="mdl-mega-footer--link-list">
+              <!--li><a href="https://JanuriDP.com">Developer</a></li>
+              <li><a href="#">Help</a></li>
+              <li><a href="#">Privacy and Terms</a></li-->
+			  <li>Prototype by <a href="https://genoffe.com/">Genoffe Studio</a></li>
+			  <li>Developed by <a href="http://januridp.com/">JanuriDP</a></li>
+			  <li>Powered by <a href="https://www.bikinhosting.com/">Bikin Hosting</a></li>
+            </ul>
+          </div>
+        </footer>
+      </main>
+    </div>
+	
+    <!-- JS -->
+    <script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  </body>
+</html>
